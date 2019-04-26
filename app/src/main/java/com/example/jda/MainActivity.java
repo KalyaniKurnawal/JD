c9b5f2d6164961145cd3113909c39ba953d98850
ARCHANA <archu.benur@gmail.com>
2019-04-26 10:37:43 +0530

    commited from JDA

--- app/src/main/java/com/example/jda/MainActivity.java
+++ app/src/main/java/com/example/jda/MainActivity.java
@@ -23,7 +23,7 @@ public class MainActivity extends AppCompatActivity {
         t2=(TextView)findViewById(R.id.txt2);
 
         t1.setText(versionName);
-        t2.setText(versionCode);
-
+        String s = Integer.toString(versionCode);
+        t2.setText(s);
     }
 }
