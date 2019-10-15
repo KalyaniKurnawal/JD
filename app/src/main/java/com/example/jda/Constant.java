e83609b8ccd3cfe921b42c129e7c1ab54ac2e107
ARCHANA <archu.benur@gmail.com>
2019-10-15 14:36:05 +0530

    commited from JDA

--- app/src/main/java/com/example/jda/Constant.java
+++ app/src/main/java/com/example/jda/Constant.java
@@ -1,8 +1,8 @@
 package com.example.jda;
 
 public class Constant {
-    static final String DTS_SDK_VERSION = "1.2.5";
+    static final String DTS_SDK_VERSION = "1.0.3";
 
     // This build number is automatically incremented by the CI
-    static final String DTS_SDK_BUILD = "25";
+    static final String DTS_SDK_BUILD = "3";
 }
